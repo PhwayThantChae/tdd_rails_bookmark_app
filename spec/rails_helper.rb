33347e05b0ffaa88b4a736b77e1fe12d01ffab55
Phway Thant Chae <ilovejazz555@gmail.com>
2022-03-25 14:07:06 +0630

    register headless chrome for capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
-Webdrivers::Chromedriver.required_version = "75.0.3770.80"
+Webdrivers::Chromedriver.required_version = "75.0.3770.140"
 
 Capybara.server = :puma, { Silent: true }
 
