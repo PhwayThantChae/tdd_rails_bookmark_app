3af68ff46c81d46326b83c0246f06357061b1410
Phway Thant Chae <ilovejazz555@gmail.com>
2022-03-25 11:43:21 +0630

    change the db adapter from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_03_25_035744) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: :cascade do |t|
     t.string "title", null: false
     t.string "url", null: false
